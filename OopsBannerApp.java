424dcaca4fe89fd17f763a0b3017f18b7ef3d4ab
vk7738 <vk7738@srmist.edu.in>
2026-02-16 21:21:13 +0300

    Add files via upload

---
+++ OopsBannerApp.java
@@ -0,0 +1,15 @@
+class OopsBannerApp {
+
+    void printOops() {
+        System.out.println("OOOOO  OOOOO  PPPPP  SSSSS");
+        System.out.println("O   O  O   O  P   P  S    ");
+        System.out.println("O   O  O   O  PPPPP  SSSSS");
+        System.out.println("O   O  O   O  P      S    ");
+        System.out.println("OOOOO  OOOOO  P      SSSSS");
+    }
+
+    public static void main(String[] args) {
+        OopsBannerApp obj = new OopsBannerApp(); // object creation
+        obj.printOops(); // method call
+    }
+}
