1d14f4bf5c849e1e40f2da291b552ea7f596e717
vk7738 <vk7738@srmist.edu.in>
2026-02-16 21:23:39 +0300

    Add files via upload

---
+++ OopsBannerUC2.java
@@ -0,0 +1,16 @@
+class OopsBannerUC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("  **      **    **    **  ");
+        System.out.println(" *     *    *     *  *     *  *     * ");
+        System.out.println("*       *  *       * *       * *       *");
+        System.out.println("*       *  *       * *       * *       *");
+        System.out.println("*       *  *       *  **    **  ");
+        System.out.println("*       *  *       *      *        * ");
+        System.out.println("*       *  *       *      *        * ");
+        System.out.println(" *     *    *     *  *     *  *     * ");
+        System.out.println("  **      **    **    **  ");
+
+    }
+}
